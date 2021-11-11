aadb01678db82e814416d07f46ffa7402896c459
Mateusz Pajewski <pajewski.m@gmail.com>
2021-11-11 15:11:20 +0100

    Chain of responsibility implementation

---
+++ behevioral-patterns/ChainOfResponsibility/Chain.cs
@@ -0,0 +1,109 @@
+﻿namespace ChainOfResponsibility;
+
+public class Chain
+{
+    private readonly Handler _chain;
+
+    public Chain()
+    {
+        _chain = BuildChain();
+    }
+
+    private Handler BuildChain()
+    {
+        return new NegativeHandler(new ZeroHandler(new PositiveHandler(null)));
+    }
+
+    public void HandleRequest(Number request)
+    {
+        _chain.HandleRequest(request);
+    }
+}
+
+abstract class Handler
+{
+    private readonly Handler? _handler;
+
+    protected Handler(Handler? handler)
+    {
+        _handler = handler;
+    }
+
+    public virtual void HandleRequest(Number request)
+    {
+        if (_handler != null)
+            _handler.HandleRequest(request);
+    }
+}
+
+public class Number
+{
+    private readonly int _number;
+
+    public Number(int number)
+    {
+        _number = number;
+    }
+
+    public int GetNumber()
+    {
+        return _number;
+    }
+}
+
+class NegativeHandler : Handler
+{
+    public NegativeHandler(Handler? handler) : base(handler)
+    {
+    }
+
+    public override void HandleRequest(Number request)
+    {
+        if (request.GetNumber() < 0)
+        {
+            Console.WriteLine("NegativeProcessor : " + request.GetNumber());
+        }
+        else
+        {
+            base.HandleRequest(request);
+        }
+    }
+}
+
+class ZeroHandler : Handler
+{
+    public ZeroHandler(Handler? handler) : base(handler)
+    {
+    }
+
+    public override void HandleRequest(Number request)
+    {
+        if (request.GetNumber() == 0)
+        {
+            Console.WriteLine("ZeroProcessor : " + request.GetNumber());
+        }
+        else
+        {
+            base.HandleRequest(request);
+        }
+    }
+}
+
+class PositiveHandler : Handler
+{
+    public PositiveHandler(Handler? handler) : base(handler)
+    {
+    }
+
+    public override void HandleRequest(Number request)
+    {
+        if (request.GetNumber() > 0)
+        {
+            Console.WriteLine("PositiveProcessor : " + request.GetNumber());
+        }
+        else
+        {
+            base.HandleRequest(request);
+        }
+    }
+}
--- behevioral-patterns/ChainOfResponsibility/Program.cs
+++ behevioral-patterns/ChainOfResponsibility/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ChainOfResponsibility;
+
+Chain chain = new Chain();
+
+chain.HandleRequest(new Number(90));
+chain.HandleRequest(new Number(-50));
+chain.HandleRequest(new Number(0));
+chain.HandleRequest(new Number(91));
+
+Console.ReadKey(); 
